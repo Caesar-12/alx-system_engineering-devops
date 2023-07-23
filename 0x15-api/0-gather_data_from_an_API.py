f3d90260928ecf5ee3befc6e069e82e26c10fa1c
Enoch <idowuenoch65@gmail.com>
2023-07-23 19:06:13 +0100

    API data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ import requests
 import sys
 
 def api_data():
-    url = 'https://jsonplaceholder.typicode.com/todos'
+    url = 'https://jsonplaceholder.typicode.com/'
     tots_task = 0
     task_comp = 0
     tasks = []
@@ -15,8 +15,10 @@ def api_data():
         sys.exit()
 
     user_id = int(sys.argv[1])
-    data = requests.get(url)
+    data = requests.get(url + 'todos')
     j_data = data.json()
+    user_data = requests.get(url +'users/{}'.format(str(user_id))).json()
+    name = user_data['name']
     #print(j_data)
 
     for dic in j_data:
@@ -26,11 +28,8 @@ def api_data():
             if dic['completed']:
                 tasks.append(dic['title'])
                 task_comp += 1
-        else:
-            print("User id not found...")
-            sys.exit()
 
-    print("Employee Patricia is done with tasks({}/{}):".format(task_comp, tots_task))
+    print("Employee {} is done with tasks({}/{}):".format(name,task_comp, tots_task))
     for task in tasks:
         print("\t {}".format(task))
 
